96b2facdb5e29c1ad04a1717c8cf8733127182af
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-10 21:22:33 +0200

    Bolas se cambian de color

--- src/Circulo.js
+++ src/Circulo.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
 export const Circulo = () => {
-  const [seleccionado, setSeleccionado] = useState(false);
-  const comprobarSeleccionado = () => {
-    if (seleccionado) {
-      return "#f00";
-    } else {
-      return "";
-    }
+  const [seleccionado, setSeleccionado] = useState(true);
+  const [backgroundColor, setBackgroundColor] = useState("#d3d3d3");
+  const cambiarFondo = () => {
+    seleccionado ? setSeleccionado(false) : setSeleccionado(true);
+    seleccionado
+      ? setBackgroundColor("#ffa500")
+      : setBackgroundColor("#d3d3d3");
   };
   return (
     <li
       className="circulo"
-      style={{ backgroundColor: { comprobarSeleccionado } }}
+      style={{ backgroundColor: backgroundColor }}
+      onClick={cambiarFondo}
     ></li>
   );
 };
